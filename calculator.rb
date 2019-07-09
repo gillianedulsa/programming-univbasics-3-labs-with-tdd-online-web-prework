7668a982400ddb4791b6511eb924abc2e6f0944c
Maria Guillian A. Edulsa <edulsag03@gmail.com>
2019-07-09 22:46:49 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,2 +1,4 @@
-expect(first_number).to be_a(57)
-expect(second_number).
+expect(first_number).to be_an(50)
+expect(second_number).to be_an (10)
+expect(second_number).not_to equal (0)
+expect(third_number). to be the_sum
